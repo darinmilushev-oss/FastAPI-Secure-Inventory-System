33ad0225746a0198c226d4daf30b0bbabd4c329c
Darin <darinmilushev@gmail.com>
2026-05-16 16:11:34 +0300

    Clean up old files and adding new endpoints and models

--- house.py
+++
@@ -1,9 +0,0 @@
-name = input("What's ypur name? ")
-
-match name:
-    case "Harry" | "Marto":
-        print("Gryffindor")
-    case "Darin":
-        print("Slitheryn")
-    case _:
-        print("Who?")
--- upr.py
+++
@@ -1,9 +0,0 @@
-score = int(input("Score:"))
-if score <= 100 and score >= 90:
-    print("Grade A")
-elif score < 90 and score >= 80:
-    print("grade B")
-elif score < 80 and score >= 70:
-    print("grade C")
-else:
-    print("Error")
